afc674137ad479ff97fd0719fd1b1cbe7c51f472
cookedas1 <mulmando1991@gmail.com>
2025-05-07 14:52:29 +0900

    README와 log 수정

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ const port = 5000;
 
 // 간단한 API 예시
 app.get("/", (req, res) => {
-  res.send("백엔드 서버가 정상적으로 작동합니다!");
+  res.send("backend server on.");
 });
 
 app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
+  console.log(`Server listening "http://localhost:${port}"`);
 });
--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,8 @@
 # frontend
+- Made by [cookedas1](https://github.com/cookedas1)
 
-## run
-1. enter "npm run start" in command prompt.
+## 프론트엔드 실행 방법
+본 단계는 프론트엔드를 실행하는 단계 및 방법을 설명합니다.
+
+1. command prompt에서 npm install를 칩니다.
+2. 그 다음으로 npm run start를 칩니다.
